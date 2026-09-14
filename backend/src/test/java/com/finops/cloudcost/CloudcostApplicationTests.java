14f0429ffb06ea290eebb149ad9f904a2b80cefa
Neelam <74339275+neelamkumari31@users.noreply.github.com>
2026-09-14 20:27:07 +0530

    Remove lines in test class

--- backend/src/test/java/com/finops/cloudcost/CloudcostApplicationTests.java
+++ backend/src/test/java/com/finops/cloudcost/CloudcostApplicationTests.java
@@ -1,13 +1,10 @@
 package com.finops.cloudcost;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CloudcostApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
